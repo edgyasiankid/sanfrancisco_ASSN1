53cd9b39030689e42b7010e3c13f199d9a2ac902
renzjulien <114795151+edgyasiankid@users.noreply.github.com>
2025-03-14 18:40:08 -0800

    Use compressed CSV and update mlp.py

--- mlp.py
+++ mlp.py
@@ -8,7 +8,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 
 #load dataset
-df = pd.read_csv('./csv/crimeTime.csv.gz', compression='gzip')
+df = pd.read_csv('./csv/crimeTime.csv')
+df.to_csv('./csv/crimeTime.csv.gz', index=False, compression='gzip')
 
 #one-hot encode 'Category' (crime type) for classification
 encoder = OneHotEncoder(sparse_output=False)
